9cd40f9cdc01c5deae40baeaaafe7b76acae2ea5
joshuaretallick <josh@old-empire.co.uk>
2021-03-25 19:31:32 +0000

    Added animation to welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  root 'home#index'
+  root 'welcome#index'
   get 'one', to: 'one#index'
   get 'two', to: 'two#index'
   get 'three', to: 'three#index'
   get 'oldempire', to: 'oldempire#index'
+  get 'home', to: 'home#index'
 end
