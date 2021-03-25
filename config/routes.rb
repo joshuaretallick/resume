5fb6e634c438e482f55ad2744dfd993d832b4825
joshuaretallick <josh@old-empire.co.uk>
2021-03-25 20:07:24 +0000

    Refer back

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  root 'welcome#index'
+  root 'home#index'
   get 'one', to: 'one#index'
   get 'two', to: 'two#index'
   get 'three', to: 'three#index'
   get 'oldempire', to: 'oldempire#index'
-  get 'home', to: 'home#index'
 end
